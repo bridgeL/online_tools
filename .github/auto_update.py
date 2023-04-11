91f7adc6d118b813f068a0c6ce392b289368f996
Su <wxlxy316@163.com>
2023-04-11 17:29:08 +0800

    更强力的自动更新日期流程

--- .github/auto_update.py
+++ .github/auto_update.py
@@ -1,16 +1,28 @@
+from datetime import datetime
 import json
 from pathlib import Path
+import re
 
-
-path = Path("readme.md")
-text = path.read_text("utf8")
+# readme.md
+readme_path = Path("readme.md")
+text = readme_path.read_text("utf8")
 text = text.split("## 功能一览")[0]
 text += "## 功能一览\n\n分类|功能|更新日期\n-|-|-\n"
 
-urls_path = Path("urls.json")
+urls_path = Path("configs.json")
 data = json.loads(urls_path.read_text("utf8"))
+data = data["urls"]
 
 for d in data:
     text += f'{d["type"]}|[{d["name"]}](https://bridgel.github.io/online_tools/{d["url"]})|{d["date"]}\n'
 
-path.write_text(text, "utf8")
+time_s = datetime.now().strftime("%Y/%m/%d")
+text = re.sub(r"<i>更新日期：.*?</i>", f"<i>更新日期：{time_s}</i>", text)
+
+readme_path.write_text(text, "utf8")
+
+# index.html
+index_path = Path("index.html")
+text = index_path.read_text("utf8")
+text = re.sub(r"<i>更新日期：.*?</i>", f"<i>更新日期：{time_s}</i>", text)
+index_path.write_text(text, "utf8")
