6d34701a610a2afab9a65dcf9b77604909eb06f4
Su <wxlxy316@163.com>
2023-04-12 11:49:53 +0800

    测试

--- .github/auto_update.py
+++ .github/auto_update.py
@@ -24,5 +24,8 @@ readme_path.write_text(text, "utf8")
 # index.html
 index_path = Path("index.html")
 text = index_path.read_text("utf8")
+print(text)
+print()
 text = re.sub(r"<i>更新时间.*?</i>", f"<i>更新时间：{time_s}</i>", text)
+print(text)
 index_path.write_text(text, "utf8")
